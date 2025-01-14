e7ed2232bcd97a29c987c8b4f3810b1732ea637d
EarningsCall <dev@earningscall.biz>
2025-01-13 20:25:35 -0600

    Add initial implementation of Python Package Index Generator
    - Created action.yml to define a GitHub Action for generating an index.html file for a private Python package repository.
    - Added generate-index.py script to handle S3 bucket name input.
    - Updated README.md with a brief description of the package functionality.
    - Established CI workflow in ci.yml for testing and publishing the package to PyPI.

---
+++ README.md
@@ -0,0 +1,2 @@
+
+# This package is used to generate the index.html file for the python package index.
---
+++ generate-index.py
@@ -0,0 +1,9 @@
+import argparse
+
+
+parser = argparse.ArgumentParser(description='')
+parser.add_argument("--s3-bucket-name", type=str, help="S3 bucket name")
+
+
+print("Hello World!")
+
